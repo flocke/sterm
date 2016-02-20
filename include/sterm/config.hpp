206f530a5bb087f141401eaa43e510a15b9ca791
Jakob Nixdorf <flocke@shadowice.org>
2016-02-20 15:20:26 +0100

    Initial commit, start of the rewrite
    This adds a first working version of the sterm::config class to parse
    and store the configuration of the terminal.

---
+++ include/sterm/config.hpp
@@ -0,0 +1,105 @@
+/*
+  Copyright (c) 2016 Jakob Nixdorf <flocke@shadowice.org>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+#ifndef __STERM_CONFIG_HPP
+#define __STERM_CONFIG_HPP
+
+#define PALETTE_SIZE 16
+
+#include <string>
+#include <vector>
+
+#include <vte/vte.h>
+
+namespace sterm {
+
+  struct color {
+    GdkRGBA               value;
+    bool                  set;
+
+    color() { set = false; }
+  };
+
+  class config {
+    private:
+      gint                    m_scrollback_lines = 0;
+      gboolean                m_allow_bold = true;
+      gboolean                m_audible_bell = false;
+      gboolean                m_scroll_on_keystroke = true;
+      gboolean                m_scroll_on_output = false;
+      gboolean                m_rewrap_on_resize = false;
+      gboolean                m_autohide_mouse = false;
+      std::string             m_encoding = "UTF-8";
+
+      VteCursorBlinkMode      m_cursor_blink_mode = VTE_CURSOR_BLINK_SYSTEM;
+      VteCursorShape          m_cursor_shape = VTE_CURSOR_SHAPE_BLOCK;
+
+      color                   m_foreground_color;
+      color                   m_background_color;
+
+      std::vector<GdkRGBA>    m_color_palette;
+
+      PangoFontDescription   *m_font = NULL;
+
+      bool                    inifile_read_string(GKeyFile *keyfile, std::string section, std::string key, std::string *target);
+      bool                    inifile_read_gboolean(GKeyFile *keyfile, std::string section, std::string key, gboolean *target);
+      bool                    inifile_read_gint(GKeyFile *keyfile, std::string section, std::string key, gint *target);
+
+      bool                    inifile_read_cursor_shape(GKeyFile *keyfile, std::string section, std::string key, VteCursorShape *target);
+      bool                    inifile_read_cursor_blink_mode(GKeyFile *keyfile, std::string section, std::string key, VteCursorBlinkMode *target);
+
+      bool                    inifile_parse_color(GKeyFile *keyfile, std::string section, std::string key, color *target);
+      bool                    inifile_parse_color_palette(GKeyFile *keyfile, std::string section, std::string prefix, std::vector<GdkRGBA> *target);
+
+      bool                    inifile_parse_font(GKeyFile *keyfile, std::string section, std::string key, PangoFontDescription **target);
+
+    public:
+      config();
+      config(std::string filename);
+      ~config();
+
+      void                    load_from_inifile(std::string filename);
+
+      gint                    get_scrollback_lines() const { return(m_scrollback_lines); }
+      gboolean                get_allow_bold() const { return(m_allow_bold); }
+      gboolean                get_audible_bell() const { return(m_audible_bell); }
+      gboolean                get_scroll_on_keystroke() const { return(m_scroll_on_keystroke); }
+      gboolean                get_scroll_on_output() const { return(m_scroll_on_output); }
+      gboolean                get_rewrap_on_resize() const { return(m_rewrap_on_resize); }
+      gboolean                get_autohide_mouse() const { return(m_autohide_mouse); }
+      std::string             get_encoding() const { return(m_encoding); }
+
+      VteCursorBlinkMode      get_cursor_blink_mode() const { return(m_cursor_blink_mode); }
+      VteCursorShape          get_cursor_shape() const { return(m_cursor_shape); }
+
+      color                   get_foreground_color() const { return(m_foreground_color); }
+      color                   get_background_color() const { return(m_background_color); }
+
+      std::vector<GdkRGBA>    get_color_palette() const { return(m_color_palette); }
+
+      bool                    copy_font_description(PangoFontDescription **target);
+
+  };
+
+}
+
+#endif
---
+++ src/config.cpp
@@ -0,0 +1,242 @@
+/*
+  Copyright (c) 2016 Jakob Nixdorf <flocke@shadowice.org>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+#include "sterm/config.hpp"
+
+namespace sterm {
+
+  config::config() {}
+
+  config::config(std::string i_filename) {
+    this->load_from_inifile(i_filename);
+  }
+
+  config::~config() {
+    if ( ! m_color_palette.empty() )
+      m_color_palette.clear();
+
+    if ( m_font != NULL )
+      pango_font_description_free(m_font);
+  }
+
+  bool config::inifile_read_string(GKeyFile *i_keyfile, std::string i_section, std::string i_key, std::string *target) {
+    if ( i_keyfile != NULL ) {
+      GError *error = NULL;
+
+      gchar *value = g_key_file_get_string(i_keyfile, i_section.c_str(), i_key.c_str(), &error);
+
+      if ( error == NULL ) {
+        *target = value;
+        g_free(value);
+        return(true);
+      }
+      
+      g_error_free(error);
+    }
+
+    return(false);
+  }
+
+  bool config::inifile_read_gboolean(GKeyFile *i_keyfile, std::string i_section, std::string i_key, gboolean *target) {
+    if ( i_keyfile != NULL ) {
+      GError *error = NULL;
+
+      gboolean value = g_key_file_get_boolean(i_keyfile, i_section.c_str(), i_key.c_str(), &error);
+
+      if( error == NULL ) {
+        *target = value;
+        return(true);
+      }
+
+      g_error_free(error);
+    }
+
+    return(false);
+  }
+
+  bool config::inifile_read_gint(GKeyFile *i_keyfile, std::string i_section, std::string i_key, gint *target) {
+    if ( i_keyfile != NULL ) {
+      GError *error = NULL;
+
+      gint value = g_key_file_get_integer(i_keyfile, i_section.c_str(), i_key.c_str(), &error);
+
+      if ( error == NULL ) {
+        *target = value;
+        return(true);
+      }
+      
+      g_error_free(error);
+    }
+
+    return(false);
+  }
+
+  bool config::inifile_read_cursor_shape(GKeyFile *i_keyfile, std::string i_section, std::string i_key, VteCursorShape *i_target) {
+    if ( i_keyfile != NULL ) {
+      std::string temp;
+
+      if ( this->inifile_read_string(i_keyfile, i_section, i_key , &temp) ) {
+
+        if ( temp.compare("block") == 0 ) {
+          *i_target = VTE_CURSOR_SHAPE_BLOCK;
+        } else if ( temp.compare("beam") == 0 ) {
+          *i_target = VTE_CURSOR_SHAPE_IBEAM;
+        } else if ( temp.compare("underline") == 0 ) {
+          *i_target = VTE_CURSOR_SHAPE_UNDERLINE;
+        } else {
+          return(false);
+        }
+
+        return(true);
+      }
+    }
+
+    return(false);
+  }
+
+  bool config::inifile_read_cursor_blink_mode(GKeyFile *i_keyfile, std::string i_section, std::string i_key, VteCursorBlinkMode *i_target) {
+    if ( i_keyfile != NULL ) {
+      std::string temp;
+
+      if ( this->inifile_read_string(i_keyfile, i_section, i_key, &temp) ) {
+
+        if ( temp.compare("on") == 0 ) {
+          *i_target = VTE_CURSOR_BLINK_ON;
+        } else if ( temp.compare("off") == 0 ) {
+          *i_target = VTE_CURSOR_BLINK_OFF;
+        } else if ( temp.compare("system") == 0 ) {
+          *i_target = VTE_CURSOR_BLINK_SYSTEM;
+        } else {
+          return(false);
+        }
+
+        return(true);
+      }
+    }
+
+    return(false);
+  }
+
+  bool config::inifile_parse_color(GKeyFile *i_keyfile, std::string i_section, std::string i_key, color *i_target) {
+    if ( i_keyfile != NULL ) {
+      std::string temp;
+
+      if ( this->inifile_read_string(i_keyfile, i_section, i_key, &temp) )
+        if ( gdk_rgba_parse(&(i_target->value), temp.c_str()) ) {
+          i_target->set = true;
+
+          return(true);
+        }
+    }
+
+    return(false);
+  }
+
+  bool config::inifile_parse_color_palette(GKeyFile *i_keyfile, std::string i_section, std::string i_prefix, std::vector<GdkRGBA> *i_target) {
+    if ( i_keyfile != NULL ) {
+      if ( ! i_target->empty() )
+        i_target->clear();
+
+      for ( int i = 0; i < PALETTE_SIZE; i++ ) {
+        color temp;
+        
+        std::string key = i_prefix + std::to_string(i);
+        if ( this->inifile_parse_color(i_keyfile, i_section, key.c_str(), &temp) )
+          i_target->push_back(temp.value);
+      }
+
+      if ( i_target->size() != PALETTE_SIZE ) {
+        i_target->clear();
+        g_warning("color palette not complete, falling back on the default colors");
+
+        return(false);
+      }
+
+      return(true);
+    }
+    
+    return(false);
+  }
+
+  bool config::inifile_parse_font(GKeyFile *i_keyfile, std::string i_section, std::string i_key, PangoFontDescription **i_target) {
+    if ( i_keyfile != NULL ) {
+      std::string temp;
+
+      if ( this->inifile_read_string(i_keyfile, i_section, i_key, &temp) ) {
+        *i_target = pango_font_description_from_string(temp.c_str());
+
+        if ( *i_target != NULL )
+          return(true);
+      }
+    }
+
+    return(false);
+  }
+
+  void config::load_from_inifile(std::string i_filename) {
+    GError *error = NULL;
+
+    GKeyFile *keyfile = g_key_file_new();
+    g_key_file_load_from_file(keyfile, i_filename.c_str(), G_KEY_FILE_NONE, &error);
+
+    if ( error != NULL ) {
+      keyfile = NULL;
+      g_error_free(error);
+
+      return;
+    }
+
+    this->inifile_read_gint(keyfile, "general", "scrollback", &m_scrollback_lines);
+    this->inifile_read_gboolean(keyfile, "general", "allow_bold", &m_allow_bold);
+    this->inifile_read_gboolean(keyfile, "general", "audible_bell", &m_audible_bell);
+    this->inifile_read_gboolean(keyfile, "general", "scroll_on_keystroke", &m_scroll_on_keystroke);
+    this->inifile_read_gboolean(keyfile, "general", "scroll_on_output", &m_scroll_on_output);
+    this->inifile_read_gboolean(keyfile, "general", "rewrap_on_resize", &m_rewrap_on_resize);
+    this->inifile_read_gboolean(keyfile, "general", "autohide_mouse", &m_autohide_mouse);
+    this->inifile_read_string(keyfile, "general", "encoding", &m_encoding);
+
+    this->inifile_read_cursor_shape(keyfile, "cursor", "shape", &m_cursor_shape);
+    this->inifile_read_cursor_blink_mode(keyfile, "cursor", "blink", &m_cursor_blink_mode);
+
+    this->inifile_parse_color(keyfile, "theme", "foreground", &m_foreground_color);
+    this->inifile_parse_color(keyfile, "theme", "background", &m_background_color);
+
+    this->inifile_parse_color_palette(keyfile, "theme", "color", &m_color_palette);
+
+    this->inifile_parse_font(keyfile, "theme", "font", &m_font);
+
+    if ( keyfile != NULL ) {
+      g_key_file_free(keyfile);
+    }
+  }
+
+  bool config::copy_font_description(PangoFontDescription **i_target) {
+    if ( m_font != NULL ) {
+      *i_target = pango_font_description_copy(m_font);
+      return(true);
+    }
+
+    return(false);
+  }
+
+}
+
